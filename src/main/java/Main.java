f39611fb1b74726f04df3c121f03464496345a31
RobertoTM2 <103004059+RobertoTM2@users.noreply.github.com>
2024-11-12 10:00:44 +0000

    Initial implementation of TSV reader and histogram generation

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./title.basics.tsv"));
+        reader.readLine();
+        Map<String, Integer> histogram = new HashMap<>();
+
+        while (true){
+            String line = reader.readLine();
+            if (line == null) break;
+            String[] fields = line.split("\t");
+            histogram.putIfAbsent(fields[1], 0);
+            histogram.compute(fields[1], (k, v) -> v + 1);
+        }
+
+        reader.close();
+
+        System.out.println(histogram);
+    }
+}
